b084b6f4f8fc444a5319dffb682c8a62aac6bc6f
edaayan0 <148155150+edaayan0@users.noreply.github.com>
2024-10-30 02:12:47 +0300

    Add files via upload
    I wrote this code to aid my German learning process. This code helps me find out if the new word I just learned exists in any of the AnnenMayKantereit songs I entered. If it does, it tells me which one. It’s a very simple code, and we can't really say it’s highly functional, but as a beginner, I wrote this code, and honestly, it has been helpful to me.

---
+++ myfirstcode.py
@@ -0,0 +1,201 @@
+dubistanders="""Du bist anders
+Du hörst mir nicht zu
+Du weißt, ich kann das
+Schweigen nicht so gut wie du
+Weil du mir nicht alles sagst
+Frag ich dich nochmal und du sagst nichts
+Weil du mir nicht alles sagst
+Frag ich dich nochmal und du sagst nichts
+Aber ich seh' was in deinem Gesicht
+Und ich weiß nicht, was es ist
+Aber ich seh' was in deinem Gesicht
+Und ich weiß nicht, was es ist
+Dadada dadada da dada
+Dadada dada dada
+Du bist anders
+Du hörst mir nicht zu
+Du weißt, ich kann das
+Schweigen nicht so gut wie du
+Ich weiß nicht, was es ist
+Und es macht mich wahnsinnig
+Und ich glaub' dir nicht
+Dass da gar nichts ist
+Ich frag mich ob du Angst hast
+Oder ist da jemand anders?
+Fällt dir auf wie wenig du lachst?
+Bitte! Bitte!
+Sag es mir und tu nicht so, als ob da gar nichts ist
+Bitte! Bitte!
+Sag es mir und tu nicht so, als ob da gar nichts ist
+Aber ich seh' was in deinem Gesicht
+Und ich weiß nicht, was es ist
+Aber ich seh' was in deinem Gesicht
+Und ich weiß nicht, was es ist
+Dadada dadada da dada
+Dadada dada dada
+Dadada dadada da dada
+Dadada dadada dada
+Weil du mir nicht alles sagst
+Frag ich dich nochmal und du sagst nichts
+Weil du mir nicht alles sagst
+Frag ich dich nochmal und du sagst nichts
+Weil du mir nicht alles sagst
+Frag ich dich nochmal und du sagst nichts
+Weil du mir nicht alles sagst
+Frag ich dich nochmal und du sagst nichts
+Weil du mir nicht alles sagst
+Frag ich dich nochmal und du sagst nichts
+Weil du mir nicht alles sagst
+Frag ich dich nochmal und du sagst nichts"""
+
+lasseskreisen="""Du musst gar nicht so viel sagen
+Du musst gar nicht so viel reden
+Du musst dich einfach nur mal wieder so bewegen, wie du willst
+Du musst dich einfach nur mal wieder bewegen, so wie du es grade fühlst
+So viele Monate alleine, ja, ich glaube, übern Winter war dir alles viel zu viel
+Lass es raus, lass es kreisen
+Lass es raus, lass es kreisen
+Lass es raus, lass es kreisen
+Ich will inmitten der Menge steh'n
+Und dich durch das Gedränge seh'n
+Ich will inmitten der Menge steh'n, die Lichter sind hell
+Und dich durch das Gedränge seh'n, verschwende mein Geld
+Angenehm untergeh'n in der Masse der Maskierten
+Ich will mal wieder mit dir die Kontrolle verlier'n
+Ich will mal wieder mit dir die Kontrolle verlier'n
+Lass es raus, lass es kreisen
+Lass es raus, lass es kreisen
+Lass es raus, lass es kreisen
+In diesen seltsamen Zeiten
+Und wenn du fällst, fang ich dich auf
+Und wenn du fällst, fang ich dich auf
+Und wenn du fällst, fang ich dich auf
+Und wenn du fällst
+Lass es raus, lass es kreisen
+Lass es raus, lass es kreisen
+Lass es raus (lass es raus), lass es kreisen
+Lass es raus (lass es kreisen), lass es kreisen
+Nach so vielen Monaten alleine musst du mir erst mal beweisen, dass du es noch kannst, tanz
+Nach so vielen Monaten alleine musst du mir erst mal beweisen, dass du es noch kannst, tanz
+Nach so vielen Monaten alleine musst du mir erst mal beweisen, dass du es noch kannst, tanz"""
+
+alsich="""Als ich ein Kind war, gab's kein Internet
+Es gab keinen Bildschirm neben dem Bett
+Das Telefon hatte ein Kabel
+Und wenn ich Fernseh'n schau'n wollte, musste ich fragen
+Die Post war gelb und keiner kannte DHL
+Alles war so langsam oder eher nicht so schnell
+Als ich ein Kind war, gab es kein Insta
+Dafür jeden Tag ab drei RTL2
+Als ich ein Kind war, gab es kein Insta
+Dafür jеden Tag ab drei RTL2
+Als ich ein Kind war, war Linda de Mol
+Einе berühmte Frau, jawohl
+Es war nicht alles gut, damals ICQ war schon ganz nice
+Früher war'n die Wiesen grün und die Bull'n auch
+Als ich ein Kind war, gab es kein Insta
+Dafür jeden Tag ab drei RTL2
+Als ich ein Kind war, gab es kein Insta
+Dafür jeden Tag ab drei RTL2
+Dafür jeden Tag ab drei RTL2"""
+
+ausgehen="""Ich weiß gar nicht so genau
+Warum ich sie so gerne mag
+Ich glaub', ich mag die Art
+Deswegen hab' ich sie gefragt
+Würdest du heute mit mir ausgehen?
+Ich würd dich auch nach Haus bringen
+Ich weiß, du musst früh aufstehen
+Würdest du trotzdem mit mir ausgehen?
+Ich glaub', ich mag die Art
+Dass sie immer am Start ist
+Wenn jemand was sagt, was
+Ihr nicht gefällt
+Da-da-da-da-da
+Ich glaube auf der Welt
+Gibt es nicht viele
+Mit so großen Zielen
+Deswegen hab' ich sie gefragt
+Würdest du heute mit mir ausgehen?
+Ich würd' dich auch nach Haus bringen
+Ich weiß, du musst früh aufstehen
+Würdest du trotzdem mit mir ausgehen?
+Würdest du heute mit mir ausgehen?
+Ich würd' dich auch nach Haus bringen
+Ich weiß, du musst früh aufstehen
+Willst du nicht trotzdem mit mir ausgehen?
+Verdammt, ich mag die Art (verdammt, ich mag die Art)
+Sie ist einfach da (sie ist einfach da)
+Und naja (naja)
+Zum Glück hab' ich sie gefragt
+Würdest du heute mit mir ausgehen?
+Ich würd' dich auch nach Haus bringen
+Ich weiß, du musst früh aufstehen
+Würdest du trotzdem mit mir ausgehen?
+Würdest du heute mit mir ausgehen?"""
+
+veilleicht="""Es ist so leicht ehrlich zu dir zu sein
+Es ist so einfach, wenn ich sage, „nein“
+Oder: „Ich bin noch nicht so weit“
+Du gibst mir Zeit
+Und du bist süß
+Wie du aussiehst, wenn du genießt
+Dass es im Winter endlich schneit
+Du magst jede Jahreszeit
+Und morgens muss ich lächeln
+Weil mich deine Küsse wecken
+Und dann merk' ich, es ist leicht
+Weil dir so wenig reicht
+Du gibst mir Zeit
+Und dann denk' ich, dass es vielleicht, vielleicht
+Für immer so bleibt
+Und dann denk' ich, dass es vielleicht, vielleicht
+Für immer so bleibt
+Ja, es ist leicht, leicht, leicht, leicht
+Ja, es ist leicht, leicht, leicht, leicht
+Es ist so schön
+Dir zuzusehen
+Wenn du duschen gehst
+Und dann vor mir stehst
+Und sagst: „Es wird ein langer Tag“
+Und wenn du mich dann fragst
+Was wir heut Abend machen
+Und wir zwischen Küssen lachen
+Dann merk' ich, es ist leicht
+Weil dir so wenig reicht
+Du gibst mir Zeit
+Und dann denk' ich, dass es vielleicht, vielleicht
+Für immer so bleibt
+Und dann denk' ich, dass es vielleicht, vielleicht
+Für immer so bleibt
+Ja, es ist leicht, leicht, leicht, leicht
+Ja, es ist leicht, leicht, leicht, leicht
+Weil du siehst, was ich sonst niemandem zeige
+Ich kann dir erzählen, was ich mir selbst verschweige
+Und ich will, dass es für immer so bleibt
+Und ich glaube, das geht
+Vielleicht, vielleicht
+Vielleicht, vielleicht
+Vielleicht, vielleicht
+Vielleicht, vielleicht
+Und ich will, dass es für immer so bleibt
+Und ich glaube, das geht
+Vielleicht, vielleicht
+Vielleicht, vielleicht
+Vielleicht, vielleicht
+Vielleicht, vielleicht
+"""
+kelime=str(input("Enter the word or the expression:"))
+
+if kelime in lasseskreisen:
+    print("Lass es kreisen")
+if kelime in dubistanders:
+    print("Du bist anders")
+if kelime in alsich:
+    print("Als ich ein Kind war")
+if kelime in ausgehen:
+    print("Ausgehen")
+if kelime in veilleicht:
+    print("Veilleicht Veilleicht")
+else:
+    print("not found")
